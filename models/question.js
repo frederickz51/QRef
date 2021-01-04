a49867ee4ace59ee8648891dbc27962706b06060
Frederick Zhu <zhu.yuming@outlook.com>
2021-01-04 15:10:57 -0600

    Added timestamp to questions db schema

--- models/question.js
+++ models/question.js
@@ -4,6 +4,6 @@ const Schema = mongoose.Schema;
 const QuestionSchema = new Schema({
     title: String,
     content: String
-});
+}, { timestamps: true });
 
 module.exports = mongoose.model('Question', QuestionSchema);
