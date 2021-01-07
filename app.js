d7a6008db7332834d400163c006f2d8763b8d19f
Frederick Zhu <zhu.yuming@outlook.com>
2021-01-06 19:45:03 -0600

    Added error page template

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path')
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const catchAsync = require('./utils/CatchAsync')
+const ExpressError = require('./utils/ExpressError')
 const methodOverride = require('method-override')
 const Question = require('./models/question');
 
@@ -33,41 +35,55 @@ app.get('/', (req, res) => {
 });
 
 
-app.get('/questions', async (req, res) => {
+app.get('/questions', catchAsync(async (req, res) => {
     const questions = await Question.find({});
     res.render('questions/index', { questions })
-});
+}));
 
-app.get('/questions/ask', async (req, res) => {
+app.get('/questions/ask', catchAsync(async (req, res) => {
     res.render('questions/ask')
-});
-app.post('/questions', async (req, res) => {
+}));
+
+app.post('/questions', catchAsync(async (req, res, next) => {
+    if (!req.body.question) throw new ExpressError("Invalid Question Datad", 400);
     const question = new Question(req.body.question)
     await question.save()
     res.redirect(`/questions/${question._id}`)
-});
+}));
 
-app.get('/questions/:id', async (req, res) => {
+
+app.get('/questions/:id', catchAsync(async (req, res) => {
     const question = await Question.findById(req.params.id)
     res.render('questions/show', { question })
-});
+}));
 
-app.get('/questions/:id/edit', async (req, res) => {
+app.get('/questions/:id/edit', catchAsync(async (req, res) => {
     const question = await Question.findById(req.params.id)
     res.render('questions/edit', { question })
-});
+}));
 
-app.put('/questions/:id', async (req, res) => {
+app.put('/questions/:id', catchAsync(async (req, res) => {
     const { id } = req.params
-    const question = await Question.findByIdAndUpdate(id, {... req.body.question})
+    const question = await Question.findByIdAndUpdate(id, { ...req.body.question })
     res.redirect(`/questions/${question._id}`)
-})
+}));
 
-app.delete('/questions/:id', async (req, res) => {
+app.delete('/questions/:id', catchAsync(async (req, res) => {
     const { id } = req.params
     await Question.findByIdAndDelete(id)
     res.redirect('/questions')
-})
+}));
+
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError("Page Not Found", 404))
+});
+
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err
+    if (!err.message) err.message = "Unknown Error"
+    res.status(statusCode).render('error', { err })
+});
 
 app.listen(3000, () => {
     console.log('Serving on port 3000.')
